179ec1803fdb9a779d6126e5fce8b33e21f5bafd
Artem Kotelnikov <artem@innot20.ru>
2024-04-04 23:18:08 +0700

    add tests for 100% coverage

--- tests/Transport/HttpTransportFactoryTest.php
+++ tests/Transport/HttpTransportFactoryTest.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace CharlaAI\Charla\Tests\Transport;
 
 use CharlaAI\Charla\Options;
+use CharlaAI\Charla\Tests\_helpers\PropertyHelper;
 use CharlaAI\Charla\Transport\DefaultTransport;
 use CharlaAI\Charla\Transport\HttpTransportFactory;
 use Http\Discovery\Psr17FactoryDiscovery;
 use Http\Discovery\Psr18ClientDiscovery;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 
 class HttpTransportFactoryTest extends TestCase
@@ -26,5 +30,51 @@ class HttpTransportFactoryTest extends TestCase
         $transport = $factory->create(new Options('token', 'endpoint'));
 
         $this->assertInstanceOf(DefaultTransport::class, $transport);
+    }
+
+    public function factoryUsesProvidedClient(): void
+    {
+        $client = $this->createMock(ClientInterface::class);
+
+        $factory = new HttpTransportFactory(
+            $client,
+            Psr17FactoryDiscovery::findUriFactory(),
+            Psr17FactoryDiscovery::findRequestFactory(),
+            new NullLogger()
+        );
+
+        $factory->setClient($client);
+
+        $this->assertSame($client, PropertyHelper::getPrivateValue($factory, 'client'));
+    }
+
+    public function factoryUsesProvidedRequestFactory(): void
+    {
+        $requestFactory = $this->createMock(RequestFactoryInterface::class);
+        $factory = new HttpTransportFactory(
+            Psr18ClientDiscovery::find(),
+            Psr17FactoryDiscovery::findUriFactory(),
+            $requestFactory,
+            new NullLogger()
+        );
+
+        $factory->setRequestFactory($requestFactory);
+
+        $this->assertSame($requestFactory, PropertyHelper::getPrivateValue($factory, 'requestFactory'));
+    }
+
+    public function factoryUsesProvidedLogger(): void
+    {
+        $logger = $this->createMock(LoggerInterface::class);
+        $factory = new HttpTransportFactory(
+            Psr18ClientDiscovery::find(),
+            Psr17FactoryDiscovery::findUriFactory(),
+            Psr17FactoryDiscovery::findRequestFactory(),
+            $logger
+        );
+
+        $factory->setLogger($logger);
+
+        $this->assertSame($logger, PropertyHelper::getPrivateValue($factory, 'logger'));
     }
 }
---
+++ tests/_helpers/PropertyHelper.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace CharlaAI\Charla\Tests\_helpers;
+
+final class PropertyHelper
+{
+    public static function getPrivateValue(object $object, string $property)
+    {
+        $reflection = new \ReflectionClass($object);
+        $property = $reflection->getProperty($property);
+        $property->setAccessible(true);
+
+        return $property->getValue($object);
+    }
+}
